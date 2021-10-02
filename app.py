ca5512c9870724e605cda91947a8fe4abdf63a5d
hamid <hamidq786@hotmail.com>
2021-10-02 03:04:56 +0100

    updates with pydantic validators

--- app.py
+++ app.py
@@ -1,60 +1,139 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import redirect
 from data import order
 from datetime import datetime
+from pydantic import BaseModel, ValidationError, validator
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
   country_list = {'GB','FR', 'DE', 'NL', 'BE', 'Other'}
-  # today = date.today()
-  # today_date = today.strftime('%b-%d-%Y')
-  new_id = len(order)+1
-  print({new_id: request.form})
-  if "name" in request.form:
-    if request.form["insurance_required"].lower() == "true" and int(request.form["value"]) <= 10000:
-        cost = int(request.form["value"])
-        country_delivered = request.form["r_country_code"]
-        if country_delivered == "GB":
-          cost = round(cost*0.01, 2)
-        elif country_delivered == "FR" or country_delivered == "DE" or country_delivered == "NL" or country_delivered == "BE":
-          cost = round(cost*0.015, 2)
-        else:
-          cost = round(cost*0.04, 2)
-        if cost < 9:
-          cost = 9
+
+  def name_must_contain_space(name: str) -> str:
+      if ' ' not in name:
+          raise ValueError('must contain a space')
+      return name.title()
+  
+  def address_must_contain_space(address: str) -> str:
+      if ' ' not in address:
+          raise ValueError('must contain a space')
+      return address
+
+  def must_contain_city(city: str) -> str:
+        if len(city) == 0:
+            raise ValueError('must enter a city')
+        return city.title()
+  
+  def insurance_limit(value: int) -> int:
+        if value > 10000:
+            raise ValueError('value is greater than £10000, unable to be insured')
+        return value
+
+  class SenderModel(BaseModel):
+    s_name: str
+    s_street_address: str
+    s_city: str
+
+    # validators
+    _contain_spaces_name = validator('s_name', allow_reuse=True)(name_must_contain_space)
+    _contain_spaces_address = validator('s_street_address', allow_reuse=True)(address_must_contain_space)
+    _contain_city = validator('s_city', allow_reuse=True)(must_contain_city)
+
+  class RecipientModel(BaseModel):
+    r_name: str
+    r_street_address: str
+    r_city: str
+
+    # validators
+    _contain_spaces_name = validator('r_name', allow_reuse=True)(name_must_contain_space)
+    _contain_spaces_address = validator('r_street_address', allow_reuse=True)(address_must_contain_space)
+    _contain_city = validator('r_city', allow_reuse=True)(must_contain_city)
+  
+  class PackageModel(BaseModel):
+    value: int
+    despatch_date: str
+
+    _insurance_limit = validator('value', allow_reuse=True)(insurance_limit)
     
-        ipt_included_in_charge = cost - (cost/1.12)
-        ipt_included_in_charge = round(ipt_included_in_charge, 2)
-        dateTimeObj = datetime.now()
-        order_url = "http://localhost:8080/order/" + str(new_id)
-        order[new_id] = {
-            "package": {
-              "sender": {
-                "name": request.form["name"],
-                "street_address": request.form["street_address"],
-                "city": request.form["city"],
-                "country_code":'GB'
-                },
-              "recipient": {
-                "name": request.form["r_name"],
-                "street_address": request.form["r_street_address"],
-                "city": request.form["r_city"],
-                "country_code":request.form["r_country_code"]
-                },
-              "value": request.form["value"],
-              "despatch_date": request.form["despatch_date"],
-              "contents_declaration": request.form["contents_declaration"],
-              "insurance_required": request.form["insurance_required"],
-              "tracking_reference": request.form["tracking_reference"]
-              },
-              "order_url": order_url,
-              "accepted_at": str(dateTimeObj),
-              "insurance_provided": request.form["insurance_required"],
-              "total_insurance_charge": cost,
-              "ipt_included_in_charge": ipt_included_in_charge
-            }
-        return render_template("index.html", template_order=order, country_list = country_list)
+
+  if "name" in request.form:
+    try:
+      SenderModel(
+        s_name = request.form["name"],
+        s_street_address = request.form["street_address"],
+        s_city = request.form["city"]
+      )
+      # print("accepted")
+      RecipientModel(
+        r_name = request.form["r_name"],
+        r_street_address = request.form["r_street_address"],
+        r_city = request.form["r_city"]
+      )
+      # print("accepted")
+      PackageModel(
+        value = int(request.form["value"]),
+        despatch_date = request.form["despatch_date"]
+      )
+      # print("accepted")
+    except ValidationError as e:
+      print(e)
+      return redirect(url_for('index'))
+
+    # declare variables for insurance value and country code
+    cost = int(request.form["value"])
+    country_delivered = request.form["r_country_code"]
+    
+
+    # check if insurance is to be included by customer
+    if int(request.form["insurance_required"]) == 1:
+      if country_delivered == "GB":
+        cost = round(cost*0.01, 2)
+      elif country_delivered == "FR" or country_delivered == "DE" or country_delivered == "NL" or country_delivered == "BE":
+        cost = round(cost*0.015, 2)
+      else:
+        cost = round(cost*0.04, 2)
+      if cost < 9:
+        cost = 9
+    
+    
+    # today = date.today()
+    # today_date = today.strftime('%b-%d-%Y')
+    new_id = len(order)+1
+    print({new_id: request.form})
+    ipt_included_in_charge = cost - (cost/1.12)
+    ipt_included_in_charge = round(ipt_included_in_charge, 2)
+    dateTimeObj = datetime.now()
+    order_url = "http://localhost:8080/order/" + str(new_id)
+
+    order[new_id] = {
+        "package": {
+          "sender": {
+            "name": request.form["name"],
+            "street_address": request.form["street_address"],
+            "city": request.form["city"],
+            "country_code":'GB'
+            },
+          "recipient": {
+            "name": request.form["r_name"],
+            "street_address": request.form["r_street_address"],
+            "city": request.form["r_city"],
+            "country_code":request.form["r_country_code"]
+            },
+          "value": request.form["value"],
+          "despatch_date": request.form["despatch_date"],
+          "contents_declaration": request.form["contents_declaration"],
+          "insurance_required": request.form["insurance_required"],
+          "tracking_reference": request.form["tracking_reference"]
+          },
+          "order_url": order_url,
+          "accepted_at": str(dateTimeObj),
+          "insurance_provided": request.form["insurance_required"],
+          "total_insurance_charge": cost,
+          "ipt_included_in_charge": ipt_included_in_charge
+        }
+    return render_template("index.html", template_order=order, country_list = country_list)
 
   return render_template("index.html", template_order=order, country_list = country_list)
 
